1d35979e9b663e6587484acb4d671dc0626bb3a5
apcedgalauran <edgalauran@student.apc.edu.ph>
2025-01-24 09:51:29 +0800

    update

--- vue-project/src/components/PersonalProfile.vue
+++ vue-project/src/components/PersonalProfile.vue
@@ -1,74 +1,7 @@
 <template>
-<div class="container">
-    <header>
-        <h1>Personal Profile Web Page</h1>
-    </header>
-    <main>
-        <section class="main-content">
-            <div>
-                <strong>About Me</strong>
-                <br>I'm a second-year CS student at Asia Pacific College, and overall just a chill guy.
-            </div>
-            <div>
-                <strong>Education</strong>
-                <br>My secondary studies were at Las Piñas City National Science High School, where I became a national champion in journalism.
-            </div>
-            <div>
-                <strong>Course</strong>
-                <br>I'm studying Computer Science with a specialization in Software and Systems.
-            </div>
-            <div>
-                <strong>IT Experience</strong>
-                <br>My college studies include learning Java and Python.
-            </div>
-            <div>
-                <strong>Hobbies</strong>
-                <ul>
-                    <li>Playing the guitar</li>
-                    <li>Playing sports like basketball and volleyball</li>
-                    <li>Playing online games like Valorant</li>
-                </ul>
-            </div>
-            <div>
-                <strong>Goal</strong>
-                <br>My goal is to achieve financial freedom.
-            </div>
-            <div>
-                <strong>Favorite Video</strong>
-                <br><a href="https://youtu.be/dQw4w9WgXcQ" target="_blank">Here is my favorite video of all time!</a>
-            </div>
-            <div>
-                <strong>Valorant Skills</strong>
-                <br>
-                <table>
-                    <tr>
-                        <th>Skill</th>
-                        <th>Proficiency</th>
-                    </tr>
-                    <tr>
-                        <td>Shooting</td>
-                        <td>Pro</td>
-                    </tr>
-                    <tr>
-                        <td>Strafe</td>
-                        <td>Pro</td>
-                    </tr>
-                    <tr>
-                        <td>Tracking</td>
-                        <td>Pro</td>
-                    </tr>
-                    <tr>
-                        <td>Game Sense</td>
-                        <td>Pro</td>
-                    </tr>
-                </table>
-            </div>
-        </section>
-    </main>
-    <footer>
-        Asia Pacific College
-    </footer>
-</div>
+  <div>
+    <h1>Testing PersonalProfile Component</h1>
+  </div>
 </template>
 
 <script>
