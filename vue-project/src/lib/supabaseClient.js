ddfd7b3bdfe6b6c86ef4ab8021abc0600b09eec8
apcedgalauran <edgalauran@student.apc.edu.ph>
2025-01-31 08:30:08 +0800

    edited

--- vue-project/src/lib/supabaseClient.js
+++ vue-project/src/lib/supabaseClient.js
@@ -1,3 +1,3 @@
 import { createClient } from '@supabase/supabase-js'
 
-export const supabase = createClient('https://gzwyxxtvhpqzgwlqdbsh.supabase.co', '<eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd6d3l4eHR2aHBxemd3bHFkYnNoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzgwMjU5MDQsImV4cCI6MjA1MzYwMTkwNH0.9u4l9PsYHbNYcx1dCINFdS43Nd2HwSzzpJMWAcrXBqM>')
+export const supabase = createClient('https://gzwyxxtvhpqzgwlqdbsh.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd6d3l4eHR2aHBxemd3bHFkYnNoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzgwMjU5MDQsImV4cCI6MjA1MzYwMTkwNH0.9u4l9PsYHbNYcx1dCINFdS43Nd2HwSzzpJMWAcrXBqM')
