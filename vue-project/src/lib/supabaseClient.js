1140d08788be4bb4dc6ba06c496d371414e170d9
apcedgalauran <edgalauran@student.apc.edu.ph>
2025-01-31 08:51:48 +0800

    changed url and anon key

--- vue-project/src/lib/supabaseClient.js
+++ vue-project/src/lib/supabaseClient.js
@@ -1,3 +1,3 @@
 import { createClient } from '@supabase/supabase-js'
 
-export const supabase = createClient('https://gzwyxxtvhpqzgwlqdbsh.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd6d3l4eHR2aHBxemd3bHFkYnNoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzgwMjU5MDQsImV4cCI6MjA1MzYwMTkwNH0.9u4l9PsYHbNYcx1dCINFdS43Nd2HwSzzpJMWAcrXBqM')
+export const supabase = createClient('https://apnosoahfwrxgmklgwsv.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFwbm9zb2FoZndyeGdta2xnd3N2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzgwMjY4NTUsImV4cCI6MjA1MzYwMjg1NX0.DBltgZtTB_Qv_o30-OsZ4ZmuYVJNmiUL5ruuG-Zaz34')
