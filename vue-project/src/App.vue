0c434f3ffb37554eacdce1c78c3572c92aa1b127
apcedgalauran <edgalauran@student.apc.edu.ph>
2025-02-04 12:57:42 +0800

    changed order

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,11 +1,11 @@
 <template>
 <h1>Food</h1>
   <personal-profile /> <br />
+  <food-item /> <br />
+  <food-item2 /> <br />
+  <food-item /> <br />
+  <food-item2 /> <br />
   <country/> <br/>
-  <food-item /> <br />
-  <food-item2 /> <br />
-  <food-item /> <br />
-  <food-item2 /> <br />
   <comment-form/>
   <comment/>
 </template>
